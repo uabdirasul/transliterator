cc3af3d78f3cd24364be78222829064a9e34b453
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-21 18:17:27 +0500

    add function that transliterate using state

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 import "./Main.css";
 
 class Main extends Component {
@@ -8,6 +8,7 @@ class Main extends Component {
       transliterator__text__lang: "uz_latin",
       transliterator__result__lang: "uz_cyrillic",
     };
+    this.transliterateRef = createRef();
   }
 
   handleSelect = (e) => {
@@ -15,6 +16,29 @@ class Main extends Component {
     this.setState({
       [name]: value,
     });
+  };
+
+  fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://api.from-to.uz/api/v1/transliterate",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            body: {
+              lang_from: this.state.transliterator__text__lang,
+              lang_to: this.state.transliterator__result__lang,
+              text: this.transliterateRef.current.value,
+            },
+          }),
+        }
+      );
+      const result = await response.json();
+      console.log(result, this);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
   };
 
   render() {
@@ -41,6 +65,8 @@ class Main extends Component {
                 cols="30"
                 rows="10"
                 placeholder="Jazıń..."
+                ref={this.transliterateRef}
+                onChange={this.fetchData}
               ></textarea>
             </div>
             <div className="transliterator__result">
