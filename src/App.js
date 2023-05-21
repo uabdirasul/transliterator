c006365dd094f2e1a1c85e567d3b7968c64c29a4
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-21 16:04:15 +0500

    add main section and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "./Layout/Header/Header";
+import Main from "./Layout/Main/Main";
 
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
+        <Main />
       </React.Fragment>
     );
   }
---
+++ src/Layout/Main/Main.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import "./Main.css";
+
+class Main extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      transliterator__text__lang: "uz_latin",
+      transliterator__result__lang: "uz_cyrillic",
+    };
+  }
+
+  handleSelect = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    return (
+      <main>
+        <div className="main__container container">
+          <div className="transliterator">
+            <div className="transliterator__text">
+              <select
+                className="transliterator__text__lang"
+                name="transliterator__text__lang"
+                onChange={this.handleSelect}
+                value={this.state.transliterator__text__lang}
+              >
+                <option value="uz_latin" key="uz_latin">
+                  Latin
+                </option>
+                <option value="uz_cyrillic" key="uz_cyrillic">
+                  Kirill
+                </option>
+              </select>
+              <textarea
+                className="textarea"
+                cols="30"
+                rows="10"
+                placeholder="Jazıń..."
+              ></textarea>
+            </div>
+            <div className="transliterator__result">
+              <select
+                className="transliterator__result__lang"
+                name="transliterator__result__lang"
+                onChange={this.handleSelect}
+                value={this.state.transliterator__result__lang}
+              >
+                <option value="uz_cyrillic" key="uz_cyrillic">
+                  Kirill
+                </option>
+                <option value="uz_latin" key="uz_latin">
+                  Latin
+                </option>
+              </select>
+              <textarea className="textarea" cols="30" rows="10"></textarea>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Main;
